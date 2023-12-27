fd58890b937f13bc5977861410336164690892df
ayaconsa <aya.consadole@gmail.com>
2023-12-27 19:38:35 +0900

    会社概要、利用規約、作成完了

--- NagoyameshiApp/models.py
+++ NagoyameshiApp/models.py
@@ -4,7 +4,17 @@ from django.db import models
 class Restaurant(models.Model):
     name = models.CharField(max_length=100)
     category = models.CharField(max_length=20)
+    catch_copy = models.CharField(max_length=30)
     explanation = models.CharField(max_length=200)
+    price_min = models.PositiveIntegerField()
+    price_max = models.PositiveIntegerField()
+    # ↓余裕があったら郵便番号から入力できるようにする
+    address = models.CharField(max_length=200)
+    tel = models.CharField(max_length=11)
+    open_time = models.CharField(max_length=5)
+    close_time = models.CharField(max_length=5)
+    close_day = models.CharField(max_length=5)
+    img = models.ImageField(blank=True, default="img/noImage.png")
 
 class User(models.Model):
     USERNAME_FIELD = "username"
--- nagoyameshi/urls.py
+++ nagoyameshi/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(), name="login"),
     path('logout/', views.LogoutView.as_view(), name="logout"),
     path('create/', views.CreateAccountView.as_view(), name="create"),
-
+    path('list/', views.RestaurantListView.as_view(), name="list"),
+    path('detail/', views.RestaurantDeleteView.as_view(), name="detail"),
 ]
