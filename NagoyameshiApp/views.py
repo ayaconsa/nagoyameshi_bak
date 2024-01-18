f7786c9471e30a26ea9debf9e42e8ed98f216c55
ayaconsa <aya.consadole@gmail.com>
2024-01-18 20:14:29 +0900

    viewとURLの整理

--- NagoyameshiApp/views.py
+++ NagoyameshiApp/views.py
@@ -16,20 +16,26 @@ from django.contrib.auth.models import User
 from .models import Restaurant
 from .forms import UserCreateForm
 
+# ================== ユーザー画面 ==================
 
-# **********トップページ・会社概要・利用規約**********
+# ************** 非会員でも表示できる画面 **************
 
+# トップページ
 class TopView(TemplateView):
     template_name = "NagoyameshiApp/top.html"
 
+
+# 会社概要
 class CompanyOverviewView(TemplateView):
     template_name = "NagoyameshiApp/company_overview.html"
 
+
+# 利用規約
 class TermsOfUseView(TemplateView):
     template_name = "NagoyameshiApp/terms_of_use.html"
 
-# *********認証機能*************
 
+# アカウントの新規登録
 class CreateAccountView(CreateView):
     def post(self, request, *args, **kwargs):
         form = UserCreateForm(data=request.POST)
@@ -42,60 +48,152 @@ class CreateAccountView(CreateView):
             user = authenticate(username=username, password=password)
             login(request, user)
             return redirect("/")
-        return render(request, "NagoyameshiApp/reate.html", {"form": form,})
+        return render(request, "NagoyameshiApp/account_create.html", {"form": form,})
 
     def get(self, request, *args, **kwargs):
         form = UserCreateForm(request.POST)
-        return  render(request, "NagoyameshiApp/create.html", {"form": form,})
+        return  render(request, "NagoyameshiApp/account_create.html", {"form": form,})
 
 create_account = CreateAccountView.as_view()
 
 
+# ログイン
 class LoginView(LoginView):
     form_class = AuthenticationForm
-    template_name = "NagoyameshiApp/login.heml"
-
-# 継承の順番重要
-# LoginRequiredMixinはログイン状態でのみ表示されるview → 非ログイン時はログイン画面に遷移
-class LogoutView(LoginRequiredMixin, LogoutView):
-    template_name = "NagoyameshiApp/logout.heml"
+    template_name = "NagoyameshiApp/login.html"
 
 
-# *********店舗機能****************
-    
-
-# ユーザー画面では、TemplateViewにしてしまうことも多い
+# 店舗一覧
 class RestaurantListView(TemplateView):
     model = Restaurant
-    template_name = "NagoyameshiApp/list.html"
+    template_name = "NagoyameshiApp/restaurant_list.html"
     # context_object_name = ''
     # テンプレートに渡されるオブジェクト名を任意のものに指定できる
     paginate_by = 10
     
-# ユーザー画面では、TemplateViewにしてしまうことも多い
+
+# 店舗詳細（→ 予約とレビューを会員限定にするにはどうしたらいいのか）
 class RestaurantDetailView(TemplateView):
     model = Restaurant
-    template_name = "NagoyameshiApp/detail.html"
-    
+    template_name = "NagoyameshiApp/restaurant_detail.html"
+
+
+
+# **************** 会員（非サブスク会員含む）のみ表示 *****************
+
+# ログアウト
+# 継承の順番重要
+# LoginRequiredMixinはログイン状態でのみ表示されるview → 非ログイン時はログイン画面に遷移
+class LogoutView(LoginRequiredMixin, LogoutView):
+    template_name = "NagoyameshiApp/logout.html"
+
+
+# パスワード再設定ページ
+class PasswordResettingView(LoginRequiredMixin, TemplateView):
+    template_name = "NagoyameshiApp/password_resetting.html"
+
+
+# サブスク案内
+class SubscriptionGuideView(TemplateView):
+    template_name = "NagoyameshiApp/subscription_guide.html"
     
 
-class RestaurantCreateView(LoginRequiredMixin, CreateView):
+# アカウント削除
+class DeleteAccountView(TemplateView):
+    template_name = "NagoyameshiApp/delete_account.html"
+
+
+# **************** サブスク会員のみ表示 *****************
+
+# 会員情報
+class AccountInfoView(LoginRequiredMixin, TemplateView):
+    model = User
+    template_name = "NagoyameshiApp/acount_info.html"
+
+
+# 予約一覧
+class BookingsView(TemplateView):
+    template_name = "NagoyameshiApp/bookings.html"
+
+
+# お気に入り一覧
+class FavoritesView(TemplateView):
+    template_name = "NagoyameshiApp/favorites.html"
+
+
+# お支払い方法変更
+class ChangePaymentMethodView(TemplateView):
+    template_name = "NagoyameshiApp/change_payment_method.html"
+
+
+# サブスク解約
+class UnsubscribeView(TemplateView):
+    template_name = "NagoyameshiApp/unsubscribe.html"
+
+
+
+# ================== 管理（店舗側）画面 ==================
+
+# 管理者（店舗）ログイン
+class ManageLoginView(TemplateView):
+    template_name = "NagoyameshiApp/manage/manage_login.html"
+    
+
+# 管理者（店舗）トップページ
+class ManageTopView(TemplateView):
+    template_name = "NagoyameshiApp/manage/manage_top.html"
+
+
+# 店舗情報（登録・編集・削除）
+class RestaurantInfoView(LoginRequiredMixin, TemplateView):
     model = Restaurant
     fields = '__all__' #フィールド内全件指定
     # fields = ["title", "image", "text"]のように指定することも可
+    template_name = "NagoyameshiApp/manage/restaurant_info.html"
+    
 
-class RestaurantUpdateView(LoginRequiredMixin, UpdateView):
-    model = Restaurant
-    fields = '__all__'
+# 予約確認
+class BookingConfirmationView(TemplateView):
+    template_name = "NagoyameshiApp/manage/booking_confirmation.html"
+    
 
-class RestaurantDeleteView(LoginRequiredMixin, DeleteView):
-    model = Restaurant
-    template_name = "Nagoyameshi/detail.html"
-    success_url = reverse_lazy('list')
+# レビュー確認
+class ReviewConfirmationView(TemplateView):
+    template_name = "NagoyameshiApp/manage/review_confirmation.html"
+    
+
+
+# ================== 管理者（サイト運営側）画面 ==================
+
+# 管理者（サイト運営者）ログイン
+class AdminLoginView(TemplateView):
+    template_name = "NagoyameshiApp/admin/admin_login.html"
+    
+
+# 管理者（サイト運営者）トップページ
+class AdminTopView(TemplateView):
+    template_name = "NagoyameshiApp/admin/admin_top.html"
+
+
+# カテゴリ管理
+class CategoryListView(TemplateView):
+    template_name = "NagoyameshiApp/admin/category_list.html"
+
+
+# 会員一覧
+class UserListView(TemplateView):
+    template_name = "NagoyameshiApp/admin/user_list.html"
+
+
+# 売り上げ集計ページ
+class SalesView(TemplateView):
+    template_name = "NagoyameshiApp/admin/sales.html"
+    
+    
+# 会員数集計ページ
+class UserNumberView(TemplateView):
+    template_name = "NagoyameshiApp/admin/user_number.html"
     
     
 
-# *********管理用画面*********
 
-class ManageTopView(TemplateView):
-    template_name = "Nagoyameshi/manage_top.html"
--- nagoyameshi/urls.py
+++ nagoyameshi/urls.py
@@ -5,17 +5,43 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    
     path('', views.TopView.as_view(), name="top"),
     path('company_overview/', views.CompanyOverviewView.as_view(), name="company_overview"),
     path('terms_of_use/', views.TermsOfUseView.as_view(), name="terms_of_use"),
-    path('login/', views.LoginView.as_view(), name="login"),
+
+    path('account_create/', views.CreateAccountView.as_view(), name="account_create"),
+    path('login/', views.LoginView.as_view(), name="login"),   
+    path('restaurant_list/', views.RestaurantListView.as_view(), name="restaurant_list"),
+    path('restaurant_detail/', views.RestaurantDetailView.as_view(), name="restaurant_detail"),
+
     path('logout/', views.LogoutView.as_view(), name="logout"),
-    path('create/', views.CreateAccountView.as_view(), name="create"),
-    path('list/', views.RestaurantListView.as_view(), name="list"),
-    path('detail/', views.RestaurantDetailView.as_view(), name="detail"),
-    path('detail/', views.RestaurantDeleteView.as_view(), name="delete"),
+    path('password_resetting/', views.PasswordResettingView.as_view(), name="password_resetting"),
+    path('subscription_guide/', views.SubscriptionGuideView.as_view(), name="subscription_guide"),
+    path('delete_account/', views.DeleteAccountView.as_view(), name="delete_account"),
+
+    path('account_info', views.AccountInfoView.as_view(), name="account_info"),
+    path('bookings', views.BookingsView.as_view(), name="bookings"),
+    path('favorites', views.FavoritesView.as_view(), name="favorites"),
+    path('change_payment_method', views.ChangePaymentMethodView.as_view(), name="change_payment_method"),
+    path('unsubscribe', views.UnsubscribeView.as_view(), name="unsubscribe"),
+    
+    
     
     path('manage/', views.ManageTopView.as_view(), name="manage_top"),
+    path('manage/login', views.ManageLoginView.as_view(), name="manage_login"),
+    path('manage/restaurant_info', views.RestaurantInfoView.as_view(), name="manage_restaurant_info"),
+    path('manage/booking_confirmation', views.BookingConfirmationView.as_view(), name="manage_booking_confirmation"),
+    path('manage/review_confirmation', views.ReviewConfirmationView.as_view(), name="manage_review_confirmation"),
+    
+    
+    
+    path('admin/', admin.site.urls),
+    path('admin/login', views.AdminLoginView.as_view(), name="admin_login"),
+    path('admin/top', views.AdminTopView.as_view(), name="admin_top"),
+    path('admin/category_list', views.CategoryListView.as_view(), name="admin_category_list"),
+    path('admin/user_list', views.UserListView.as_view(), name="admin_user_list"),
+    path('admin/sales', views.SalesView.as_view(), name="admin_sales"),
+    path('admin/user_number', views.UserNumberView.as_view(), name="admin_user_number"),
     
 ]
